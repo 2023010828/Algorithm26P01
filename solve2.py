f018d3acc0304e4b15d8f0512c74749f20869944
진혜진 <2023010828@gnu.ac.kr>
2026-04-01 18:31:46 +0900

    Update solve2.py

--- solve2.py
+++ solve2.py
@@ -1,7 +1,24 @@
 def solve2(format_str):
-    # Q: Readme 파일의 두 번째 문제
+
     answer = 0
-    
-    # 여기에 코드를 작성하십시오.
-    
+
+
+    for i in range(len(format_str)):
+        if i == 0:   
+            if format_str[i] == 'C':
+                answer = 26
+            else:
+                answer = 10
+        else:
+            if format_str[i] == 'C':
+                if format_str[i-1] == 'C':
+                    answer = answer * 25
+                else:
+                    answer = answer * 26
+            else:
+                if format_str[i-1] == 'N':
+                    answer = answer * 9
+                else:
+                    answer = answer * 10
+
     return answer
